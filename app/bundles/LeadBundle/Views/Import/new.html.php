555773a8e069a6a79918dd72e0ebd1fc855c61b2
Scott <scott.olipra@gmail.com>
2018-07-15 18:57:13 -0400

    added a warning message to the import screen. :  Want to fuck up a working relationship?

--- app/bundles/LeadBundle/Views/Import/new.html.php
+++ app/bundles/LeadBundle/Views/Import/new.html.php
@@ -14,6 +14,13 @@ $view['slots']->set('mauticContent', 'leadImport');
 $view['slots']->set('headerTitle', $view['translator']->trans('mautic.lead.import.leads', ['%object%' => $view['request']->getParameter('object')]));
 
 ?>
+
+<div class="scott" style="text-align: center;">
+       <h2>Want to seriously fuck up a working relationship?</h2>
+       <h3><span style="color:darkblue">Hit that blue import button</span> without first snapshotting the database.</h3>
+       <h6 style="color:#aaa">(I don't know how else to fucking say it.)</h6>
+</div>
+
 <?php if (isset($form['file'])): ?>
 <?php echo $view->render('MauticLeadBundle:Import:upload_form.html.php', ['form' => $form]); ?>
 <?php else: ?>
